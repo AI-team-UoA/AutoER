68b07e493cd00ca8c50e259df8321c5bee18ccd3
Nikoletos-K <nikoletos.konstantinos99@gmail.com>
2024-06-10 13:56:14 +0300

    fixed census exps scripts

--- benchmarks/performance_per_census_dataset.py
+++ benchmarks/performance_per_census_dataset.py
@@ -1,4 +1,4 @@
-import time
+import os
 import pandas as pd
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 
@@ -9,9 +9,14 @@ TOPK = 20
 
 #  create results csv
 
-summary_df = pd.DataFrame(columns=['dataset', 'predicted'])
+summary_df = pd.DataFrame(columns=['dataset', 'predicted', 'max'])
 
 for census_results in census_datasets:
+
+    #  if file exists
+    if not os.path.isfile("./predictions/" + census_results + "_results.csv"):
+        print("File not found: ", census_results)
+        continue
 
     results = pd.read_csv("./predictions/" + census_results + "_results.csv")
 
@@ -33,12 +38,16 @@ for census_results in census_datasets:
     print(topKtrue)
 
     LOCAL_BEST_TRUE = topKtrue['true'].max()
+    print("\n\nLocal Best True: ", LOCAL_BEST_TRUE)
+
     BEST_PREDICTED = topKpredicted['predicted'].idxmax()
+
+    print(topKpredicted.loc[BEST_PREDICTED,:])
     BEST_PREDICTED = topKpredicted.loc[BEST_PREDICTED, 'true']
 
     print("\n\nBest Predicted: ", BEST_PREDICTED)
 
-    new_row = pd.DataFrame([{'dataset': census_results, 'predicted': BEST_PREDICTED}])
+    new_row = pd.DataFrame([{'dataset': census_results, 'predicted': BEST_PREDICTED, 'max': LOCAL_BEST_TRUE}])
     summary_df = pd.concat([summary_df, new_row], ignore_index=True)
 
 summary_df.to_csv('./predictions/summary.csv', index=False)
