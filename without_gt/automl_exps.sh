a64b5ac52be19ec24f51f08c85e374eef5b4fefa
Nikoletos-K <nikoletos.kon@gmail.com>
2024-08-21 11:53:33 +0300

    Finished AutoML exps with 6 hours

--- without_gt/automl_exps.sh
+++ without_gt/automl_exps.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
 datasets=("optuna" "gridsearch" "all")
+ds=("D1" "D2" "D3" "D4" "D5" "D6" "D7" "D8" "D9" "D10")
+# datasets=("gridsearch")
+
 
 mkdir -p ./automl
 
 for dataset in "${datasets[@]}"; do
-  command="python -u regression_with_automl.py --trials $dataset"
-  log_file="./automl/logs/${dataset}.log"
-  
-  echo "Running: nohup $command > $log_file 2>&1 &"
-  nohup $command > $log_file 2>&1 &
+  for d in "${ds[@]}"; do
+
+    command="python -u regression_with_automl.py --trials $dataset --d $d "
+    log_file="./automl/logs/${dataset}_${d}.log"
+    
+    echo "Running: nohup $command > $log_file 2>&1 &"
+    nohup $command > $log_file 2>&1 &
+    sleep 15
+
+  done
   # wait
 done
 
