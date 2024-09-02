16db64022e9ab6bb20871fc2c8bc1cea52c879bd
Nikoletos-K <nikoletos.kon@gmail.com>
2024-09-02 15:11:51 +0300

    Added feauture importance file

--- without_gt/automl_exps.sh
+++ without_gt/automl_exps.sh
@@ -3,7 +3,7 @@
 datasets=("optuna" "gridsearch" "all")
 ds=("D1" "D2" "D3" "D4" "D5" "D6" "D7" "D8" "D9" "D10")
 
-mkdir -p ./automl
+mkdir -p ./automl/logs
 
 for dataset in "${datasets[@]}"; do
   for d in "${ds[@]}"; do
@@ -11,12 +11,15 @@ for dataset in "${datasets[@]}"; do
     command="python -u regression_with_automl.py --trials $dataset --d $d "
     log_file="./automl/logs/${dataset}_${d}.log"
     
+    start_time=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "[$start_time] Starting process for dataset: $dataset, d: $d" | tee -a $log_file
+
     echo "Running: nohup $command > $log_file 2>&1 &"
-    nohup $command > $log_file 2>&1 &
-    sleep 15
+    nohup $command >> $log_file 2>&1 &
+    sleep 120
 
   done
-  # wait
+  wait
 done
 
 echo "All tasks completed."
