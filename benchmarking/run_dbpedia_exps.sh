4b7d974ffb5654b4b07a04f162bc1ecfd9551222
Nikoletos-K <nikoletos.kon@gmail.com>
2024-10-01 10:40:45 +0300

    Upload DBPedia predictios - to be evaluated

---
+++ benchmarking/run_dbpedia_exps.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+timestamp() {
+  date +"%Y-%m-%d %H:%M:%S"
+}
+
+trials_types=("optuna" "gridsearch" "all")
+log_file="./logs/"
+
+echo "Script started at: $(timestamp)"
+echo "Running: DBPEDIA experiments"
+echo "Start time: $(timestamp)"
+
+for t in "${trials_types[@]}";
+do
+    echo "Starting process for trials type: $t"
+
+    t_capitalized=$(echo $t | tr '[:lower:]' '[:upper:]')
+
+    command="python eteer_evaluate.py --config ./configs/D1D10_DBPEDIA_${t_capitalized}.json"
+    log_file="./logs/D1D10_DBPEDIA_${t_capitalized}.log"
+    
+    if [ -f $log_file ]; then
+      rm $log_file
+    fi
+
+    echo "Command: $command"
+    echo "Log file: $log_file"
+
+    start_time=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "[$start_time] Starting process for trials type: $t"
+
+    echo "Running: nohup $command > $log_file 2>&1 &"
+    nohup $command >> $log_file 2>&1 &
+    echo "Process started for trials type: $t"
+    sleep 10
+done
+echo "Script ended at: $(timestamp)"
+echo "Done"
