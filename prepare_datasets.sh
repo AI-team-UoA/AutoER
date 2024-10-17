ba20dd0792f2c082ba421f9588587734169b34e7
Nikoletos-K <nikoletos.kon@gmail.com>
2024-10-17 17:46:58 +0300

    Fixed dataset preparation component

--- prepare_datasets.sh
+++ prepare_datasets.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo "Downloading datasets..."
-#curl -L -o autoer_data.tar.gz https://zenodo.org/records/13946189/files/autoer_data.tar.gz
-#curl -L -o zeroer_like_datasets.tar.gz https://zenodo.org/records/13946189/files/zeroer_like_datasets.tar.gz
+curl -L -o autoer_data.tar.gz https://zenodo.org/records/13946189/files/autoer_data.tar.gz
+curl -L -o zeroer_like_datasets.tar.gz https://zenodo.org/records/13946189/files/zeroer_like_datasets.tar.gz
 
 echo "Extracting datasets... in specific folder"
 
